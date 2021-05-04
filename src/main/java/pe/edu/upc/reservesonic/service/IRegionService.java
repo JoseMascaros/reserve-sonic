e64adcb9bba4a2aa1ce10935d6aa1e663f9a993d
cristinahf13 <adacristinahurtadoflores@gmail.com>
2021-05-04 01:10:56 -0500

    Create Interface Region Service

---
+++ src/main/java/pe/edu/upc/reservesonic/service/IRegionService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.reservesonic.service;
+
+import java.util.List;
+
+import pe.edu.upc.reservesonic.models.entities.Region;
+
+public interface IRegionService {
+	public void insert(Region region);
+	
+	public List<Region> list();
+	
+	public void delete(String id);
+	
+}
