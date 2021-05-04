1e02d533a0871be21059b0c4a988abf8252f0f65
JossMass <jmascaro2000@gmail.com>
2021-05-04 01:29:43 -0500

    Region Class updated

--- src/main/java/pe/edu/upc/reservesonic/models/entities/Region.java
+++ src/main/java/pe/edu/upc/reservesonic/models/entities/Region.java
@@ -20,12 +20,12 @@ public class Region {
 	private String name;
 	
 	//OneToMany relationships
-	@OneToMany(mappedBy = "region")
-	private List<District> districts;
+	//@OneToMany(mappedBy = "region")
+	//private List<District> districts;
 	
 	//Constructor, getters & setters
 	public Region() {
-		districts = new ArrayList<District>();
+		//districts = new ArrayList<District>();
 	}
 
 	public String getId() {
@@ -44,13 +44,13 @@ public class Region {
 		this.name = name;
 	}
 
-	public List<District> getDistricts() {
-		return districts;
-	}
+	//public List<District> getDistricts() {
+		//return districts;
+	//}
 
-	public void setDistricts(List<District> districts) {
-		this.districts = districts;
-	}
+	//public void setDistricts(List<District> districts) {
+		//this.districts = districts;
+	//}
 	
 	
 }
